90897034bd700841fd8de185d843816d36e88c0c
makarenkoj <makarenkoj@gmail.com>
2019-07-25 15:12:48 +0300

    changed minutes to seconds

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -11,7 +11,7 @@ class Quiz
     doc.elements.each('questions/question') do |item|
       question = Question.new
 
-      question.time = item.attributes['minutes'].to_i
+      question.time = item.attributes['seconds'].to_i
       question.question_text = item.elements['text'].text
 
       item.elements.each('variants/variant') do |variant|
